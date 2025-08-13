fb07c499a59ea32ac4fcd87c133be85480dc4f3b
gustavoalbertoo <gustavo2020alberto@gmail.com>
2025-08-12 22:00:34 -0300

    Create exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,15 @@
+/* Lab.1
+ * Exemplo 1 */
+
+#include <stdio.h>
+
+int main () {
+	printf("Mackenzie - FCI\n");
+	printf("Algoritmos e Prog. II\n");
+	printf("Prog. II\n");
+	printf("Turma - 02P\n");
+
+	
+	system("pause");
+	return 0;
+}
